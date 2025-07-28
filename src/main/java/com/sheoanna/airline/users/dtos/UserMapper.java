8d61c2eb7718aad5a74d4508c295a9ef587fef22
NepyAnna <nepyann@gmail.com>
2025-07-28 21:26:14 +0200

    changed UserMapper from mapstructure to manual implementation

--- src/main/java/com/sheoanna/airline/users/dtos/UserMapper.java
+++ src/main/java/com/sheoanna/airline/users/dtos/UserMapper.java
@@ -2,25 +2,33 @@ package com.sheoanna.airline.users.dtos;
 
 import com.sheoanna.airline.role.Role;
 import com.sheoanna.airline.users.User;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
+import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@Mapper(componentModel = "spring")
-public interface UserMapper {
-    @Mapping(source = "roles", target = "roles", qualifiedByName = "mapRolesToNames")
-    @Mapping(source = "profile", target = "profile")
-    UserResponse toResponse(User user);
+@Component
+public class UserMapper {
 
-    User toEntity(UserRequest request);
-
-    @Named("mapRolesToNames")
-    default Set<String> mapRolesToNames(Set<Role> roles) {
-        return roles.stream()
+    public UserResponse toResponse(User user) {
+        Set<String> roles = user.getRoles() != null
+                ? user.getRoles().stream()
                 .map(Role::getName)
-                .collect(Collectors.toSet());
+                .collect(Collectors.toSet())
+                : Collections.emptySet();
+
+        return UserResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .roles(roles)
+                .build();
+    }
+
+    public User toEntity(UserRequest request) {
+        return User.builder()
+                .username(request.username())
+                .password(request.password())
+                .build();
     }
 }
