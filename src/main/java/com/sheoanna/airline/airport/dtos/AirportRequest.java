28396bcfcc904b034893220564bd4b18163e7c90
NepyAnna <nepyann@gmail.com>
2025-07-29 15:19:32 +0200

    Added tests for AirportService, added handling to GlobalHandlerException

--- src/main/java/com/sheoanna/airline/airport/dtos/AirportRequest.java
+++ src/main/java/com/sheoanna/airline/airport/dtos/AirportRequest.java
@@ -1,3 +1,10 @@
 package com.sheoanna.airline.airport.dtos;
 
-public record AirportRequest(String name, String codeIata) {}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record AirportRequest(@NotBlank(message="Airport name is required.")
+                             String name,
+                             @NotBlank(message="Code IATA of departure airport required")
+                             @Pattern(regexp = "^[A-Z]{3}$", message = "Departure IATA code must be 3 uppercase letters")
+                             String codeIata) {}
